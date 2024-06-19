b4751492bc8c4fc804a7ef225a58a7520612564b
Lucas <lsantos2003silva@gmail.com>
2024-06-19 20:50:31 -0300

    implementing search_directory

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -1,4 +1,25 @@
-use std::fs::File;
-use std::path::Path;
+use std::fs::read_dir;
+use std::path::{Path, PathBuf};
 
-fn scan_dir(path: Path) ->
+pub fn search_directory(path_origin: &Path, key_dir: &str) -> Result<Option<PathBuf>, std::io::Error> {
+  let mut stack: Vec<PathBuf> = Vec::new();
+
+  stack.push(path_origin.to_path_buf());
+  while let Some(current_path) = stack.pop() {
+    
+    for entry in read_dir(current_path)? {
+      let entry: std::fs::DirEntry = entry?;
+      let path: PathBuf = entry.path();
+      
+      if path.is_dir() { 
+        if path.file_name().and_then(|name| name.to_str()) == Some(key_dir) {
+          return Ok(Some(path));
+        } else {
+          stack.push(path);
+        }
+      }
+    }
+  }
+
+  return Ok(None);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::Path;
+
+pub mod file_handling;
+
 fn main() {
-    println!("Hello, world!");
+	let path = Path::new("/home/dio/git_repos/just-coding/");
+	match file_handling::search_directory(path, "messing-with-apis") {
+		Ok(opt) =>  {
+			match opt {
+				Some(s) => println!("found in: {}", s.to_str().unwrap()),
+				None => println!("not found in: {}", path.to_str().unwrap()),
+			}
+		},
+
+		Err(e) => eprintln!("an error occurred: {:#?}", e),
+	};
 }
