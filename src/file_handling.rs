093a6bdbbe6610931192a952529b64c257af4d5e
Lucas <lsantos2003silva@gmail.com>
2024-06-20 01:50:55 -0300

    implementing create, read and move file functions

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -1,6 +1,8 @@
-use std::fs::read_dir;
+use core::panic;
+use std::fs::{ read_dir, remove_file, read, File };
 use std::path::{ Path, PathBuf };
-use std::io::Error;
+use std::io::{ Error, ErrorKind, Write };
+
 
 // Craw through an directory to find another directory
 pub fn search_directory(path_origin: &Path, key_dir: &str) -> Result<Option<PathBuf>, Error> {
@@ -29,6 +31,7 @@ pub fn search_directory(path_origin: &Path, key_dir: &str) -> Result<Option<Path
 // Return the files inside an directory
 pub fn open_directory(path_to: &Path) -> Result<Option<Vec<PathBuf>>, Error> {  
   let mut dir_content: Vec<PathBuf> = Vec::new();
+  
   for entry in read_dir(path_to)? {
     let entry = entry?;
     dir_content.push(entry.path());
@@ -39,4 +42,85 @@ pub fn open_directory(path_to: &Path) -> Result<Option<Vec<PathBuf>>, Error> {
   } else {
     return Ok(None);
   }
-} 
+}
+
+
+// return an string with the content of the file
+pub fn read_file(path: &Path) -> Result<Option<Vec<u8>>, Error> {
+  
+  if !path.exists() {
+    eprintln!("read_file() > Error: src {} does not exist.", path.display());
+    return Err(Error::from(ErrorKind::NotFound));
+  }
+
+  let content: Vec<u8> = match read(path) {
+    Ok(s) => s,
+    Err(e) => panic!("read_file() > could not read the file to an string: {:#?}", e ),
+  };
+
+  if content.len() > 1 {
+    Ok(Some(content))
+  } else {
+    Ok(None)
+  }
+}
+
+pub fn create_file(dest: &Path, content: &Vec<u8>) -> Result<(), Error> {
+
+  if dest.exists() {
+    eprintln!("create_file > Error: file {} already exists", dest.display());
+    return Err(Error::from(ErrorKind::AlreadyExists));
+  }
+
+  let mut file: File = match File::create(dest) {
+    Ok(f) => f,
+    Err(e) => {
+      eprintln!("create_file > Error: was not possible to create file {}, error {}", dest.display(), e);
+      return Err(e);
+    },
+  };
+
+  match file.write_all(content) {
+    Ok(()) => Ok(()),
+    Err(e) => {
+      eprintln!("create_file > Error: was not possible to create file {}, error {}", dest.display(), e);
+      Err(e)
+    },
+  }
+}
+
+
+pub fn move_file(src: &Path, dest: &Path) -> Result<(), Error> {
+  
+  if !src.exists() {
+    eprintln!("move_file() > Error: src {} does not exist.", src.display());
+    return Err(Error::from(std::io::ErrorKind::NotFound));
+  } 
+  else if dest.exists() {
+    eprintln!("move_file() > {} already exists.", dest.display());
+    return Err(Error::from(ErrorKind::AlreadyExists));
+  } 
+
+  let src_content = match read_file(src)? {
+    Some(s) => s,
+    None => { 
+      eprintln!("move_file > read_file returned None when trying to read the content of file {} ", src.display()); 
+      return Err(Error::from(ErrorKind::WriteZero));
+    }
+  };
+
+  match create_file(dest, &src_content) {
+    Ok(()) => {
+      match remove_file(src) {
+        Ok(()) => Ok(()),
+
+        Err(e) => {
+          eprintln!("move_file > an error occurred while trying to remove {}. Error: {}", src.display(), e);
+          Err(e)
+        }
+      }
+    },
+
+    Err(e) => Err(e),
+  }
+}
