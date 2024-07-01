0a47b492ef1f1557650aee3d6f623a39a5a5b64f
Lucas Santos <lsantos2003silva@gmail.com>
2024-07-01 04:59:15 -0300

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/file_handling.rs

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -1,59 +1,93 @@
-use core::panic;
-use std::fs::{ read, read_dir, remove_file, File };
-use std::path::{ Path, PathBuf };
-use std::io::{ Error, ErrorKind, Write };
+use walkdir::WalkDir;
+use std::path::Path;
+use std::collections::HashMap;
+// use std::env;
+use std::fs;
+use std::io;
+use serde_json;
 
 
-// Craw through an directory to find another directory
-pub fn search_directory(path_origin: &Path, key_dir: &str) -> Result<Option<PathBuf>, Error> {
-  let mut stack: Vec<PathBuf> = Vec::new();
+fn scan_dir_filename(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
+  let mut vec: Vec <String> = Vec::new();
+  let entries = fs::read_dir(path)?;
+  for entry in entries {
+    vec.push( match entry {
+      Ok(f) => f.path()
+        .file_name()
+        .map_or_else(String::new, 
+          |s| s.to_str()
+          .unwrap_or("")
+          .to_string()),
 
-  stack.push(path_origin.to_path_buf());
-  while let Some(current_path) = stack.pop() {
-    
-    for entry in read_dir(current_path)? {
-      let entry: std::fs::DirEntry = entry?;
-      let path: PathBuf = entry.path();
-      
-      if path.is_dir() { 
-        if path.file_name().and_then(|name| name.to_str()) == Some(key_dir) {
-          return Ok(Some(path));
+      Err(e) => { eprintln!("error: {e:#?}"); String::new() },
+    });
+  }
+
+  Ok(Some(vec))
+}
+
+pub fn scan_dir(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
+  let mut vec: Vec <String> = Vec::new();
+  let entries = fs::read_dir(path)?;
+  for entry in entries {
+    vec.push(match entry {
+      Ok(f) => f.path()
+        .to_str()
+        .map_or_else(String::new, String::from),
+
+      Err(e) => { eprintln!("error: {e:#?}"); String::new() },
+    });
+  }
+
+  Ok(Some(vec))
+}
+
+pub fn craw_dir(path: &Path) -> Result< Option <HashMap <String, Vec<String>>>, walkdir::Error> {
+  let mut map: HashMap <String, Vec<String>> = HashMap::new();
+
+  if path.exists() {
+    for entry in WalkDir::new(path).into_iter().flatten() {
+      if entry.path().is_dir() {
+        let path: &Path = entry.path();
+        let path_str: &str = path
+          .to_str()
+          .unwrap_or("");
+
+        if !map.contains_key(path_str) {
+          map.insert(path_str.to_string(), match scan_dir(Path::new(path_str)) {
+            Ok(s) => s.unwrap_or_else(Vec::new),
+            Err(e) => { eprintln!("error: {e:#?}"); Vec::new()}, 
+          });
+          
         } else {
-          stack.push(path);
+          continue;
         }
       }
     }
   }
 
-  return Ok(None);
+  Ok(Some(map))
 }
 
-// Return the files inside an directory
-pub fn open_directory(path_to: &Path) -> Result<Option<Vec<PathBuf>>, Error> {  
-  let mut dir_content: Vec<PathBuf> = Vec::new();
-  
-  for entry in read_dir(path_to)? {
-    let entry = entry?;
-    dir_content.push(entry.path());
-  }
+pub fn write_file(file_path: &Path, map: &HashMap<String, Vec<String>>) -> Result<(), serde_json::Error> {
 
-  if dir_content.len() > 0 {
-    return Ok(Some(dir_content));
-  } else {
-    return Ok(None);
-  }
+  let content = serde_json::to_string_pretty(&map).unwrap();
+  fs::write(file_path, content.as_bytes())
+    .expect("Error while trying to write content to output file.");
+
+  Ok(())
 }
 
 
 // return an Vedc<u8> with the content of the file
-pub fn read_file(path: &Path) -> Result<Option<Vec<u8>>, Error> {
+pub fn read_file(path: &Path) -> Result<Option<Vec<u8>>, io::Error> {
   
   if !path.exists() {
     eprintln!("read_file() > Error: src {} does not exist.", path.display());
-    return Err(Error::from(ErrorKind::NotFound));
+    return Err(io::Error::from(io::ErrorKind::NotFound));
   }
 
-  let content: Vec<u8> = match read(path) {
+  let content: Vec<u8> = match fs::read(path) {
     Ok(s) => s,
     Err(e) => panic!("read_file() > could not read the file to an string: {:#?}", e ),
   };
@@ -65,14 +99,14 @@ pub fn read_file(path: &Path) -> Result<Option<Vec<u8>>, Error> {
   }
 }
 
-pub fn create_file(dest: &Path, content: &Vec<u8>) -> Result<(), Error> {
+pub fn create_file(dest: &Path, content: &Vec<u8>) -> Result<(), io::Error> {
 
   if dest.exists() {
     eprintln!("create_file > Error: file {} already exists", dest.display());
-    return Err(Error::from(ErrorKind::AlreadyExists));
+    return Err(io::Error::from(io::ErrorKind::AlreadyExists));
   }
 
-  let mut file: File = match File::create(dest) {
+  let mut file: fs::File = match fs::File::create(dest) {
     Ok(f) => f,
     Err(e) => {
       eprintln!("create_file > Error: was not possible to create file {}, error {}", dest.display(), e);
@@ -105,22 +139,22 @@ pub fn create_file(dest: &Path, content: &Vec<u8>) -> Result<(), Error> {
 // }
 
 
-pub fn move_file(src: &Path, dest: &Path) -> Result<(), Error> {
+pub fn move_file(src: &Path, dest: &Path) -> Result<(), io::Error> {
   
   if !src.exists() {
     eprintln!("move_file() > Error: src {} does not exist.", src.display());
-    return Err(Error::from(std::io::ErrorKind::NotFound));
+    return Err(io::Error::from(io::ErrorKind::NotFound));
   } 
   else if dest.exists() {
     eprintln!("move_file() > {} already exists.", dest.display());
-    return Err(Error::from(ErrorKind::AlreadyExists));
+    return Err(io::Error::from(io::ErrorKind::AlreadyExists));
   } 
 
   let src_content = match read_file(src)? {
     Some(s) => s,
     None => { 
       eprintln!("move_file > read_file returned None when trying to read the content of file {} ", src.display()); 
-      return Err(Error::from(ErrorKind::WriteZero));
+      return Err(io::Error::from(io::ErrorKind::WriteZero));
     }
   };
 
